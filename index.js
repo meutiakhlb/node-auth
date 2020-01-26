02bec5c3d0b22291f557cd56871da801305ee69c
meutiakhlb <julitayuventa@gmail.com>
2020-01-26 14:35:58 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const user = require("./routes/user"); //new addition
+const InitiateMongoServer = require("./config/db");
+
+// Initiate Mongo Server
+InitiateMongoServer();
+const app = express();
+
+// PORT
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API Working" });
+});
+
+
+/**
+ * Router Middleware
+ * Router - /user/*
+ * Method - *
+ */
+app.use("/user", user);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server Started at PORT ${PORT}`);
+});
